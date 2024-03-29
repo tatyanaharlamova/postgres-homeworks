a8f4493bd0c0e95b57ef16a2d881a6b5562e321a
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-03-29 12:39:54 +0300

    Homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id	int PRIMARY KEY,
+	first_name varchar(50) NOT NUll,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+CREATE TABLE customers
+(
+	customer_id	varchar(10) PRIMARY KEY,
+	company_name varchar(50) NOT NUll,
+	contact_name varchar(50) NOT NULL
+);
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+from config import PASSWORD
+
+
+def main():
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password=PASSWORD
+    )
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                with open('north_data/customers_data.csv', 'r') as f:
+                    reader = csv.reader(f)
+                    next(reader)
+                    for row in reader:
+                        cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+                with open('north_data/employees_data.csv', 'r') as f:
+                    reader = csv.reader(f)
+                    next(reader)
+                    for row in reader:
+                        cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', row)
+                with open('north_data/orders_data.csv', 'r') as f:
+                    reader = csv.reader(f)
+                    next(reader)
+                    for row in reader:
+                        cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
